f4109fabae115950ba17badae26f69e84c6ec6e5
dgarnitz <david.garnitz@gmail.com>
2018-09-22 22:02:30 +0100

    second commit

--- MyFlaskApp.py
+++ MyFlaskApp.py
@@ -4,4 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-        return '<h1>My Flask App</h1>'
+    return '<h1>My Flask App</h1>'
+
+@app.route('/info')
+def info():
+    return '<p>This page provides info about my application</p>'
