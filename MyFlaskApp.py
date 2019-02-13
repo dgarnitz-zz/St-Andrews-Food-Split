c3bd369e6ec153ec89c9218249b5fa3d09e76605
dgarnitz <david.garnitz@gmail.com>
2019-02-13 22:37:24 +0000

    added some error handling

--- MyFlaskApp.py
+++ MyFlaskApp.py
@@ -11,3 +11,12 @@ def index():
 @app.route('/info')
 def info():
     return '<p>This page provides info about my application</p>'
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
